8d416a17cfd440f5b3216e862944633b76dec7c2
Питанов Валера <dtho47@yandex.ru>
2021-04-22 20:50:30 +1000

    add share module

---
+++ plugins/appsharedialog/README.md
@@ -0,0 +1,10 @@
+# Godot iOS InAppStore plugin
+
+## Methods
+
+`share_text(String title, String subject, String text)` - share text
+`share_image(String path, String title, String subject, String text)` - share image from path  
+  
+
+## Properties
+
---
+++ plugins/appsharedialog/app_share_dialog.mm
@@ -0,0 +1,102 @@
+/*************************************************************************/
+/*  in_app_store.mm                                                      */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#include "app_share_dialog.h"
+
+#import <Foundation/Foundation.h>
+#import <StoreKit/StoreKit.h>
+#import "app_delegate.h"
+
+AppShareDialog *AppShareDialog::instance = NULL;
+
+
+void AppShareDialog::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("share_text"), &InAppStore::share_text);
+	ClassDB::bind_method(D_METHOD("share_image"), &InAppStore::share_image);
+}
+
+AppShareDialog *AppShareDialog::get_singleton() {
+	return instance;
+}
+
+AppShareDialog::AppShareDialog() {
+	ERR_FAIL_COND(instance != NULL);
+	instance = this;
+}
+
+
+AppShareDialog::~AppShareDialog() {
+}
+
+void InAppStore::share_text(const String &title, const String &subject, const String &text) {
+	AppDelegate *app_delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+	ViewController *root_controller = (ViewController *)[app_delegate viewController];
+
+	NSString * message = [NSString stringWithCString:text.utf8().get_data() encoding:NSUTF8StringEncoding];
+
+	NSArray * shareItems = @[message];
+
+	UIActivityViewController * avc = [[UIActivityViewController alloc] initWithActivityItems:shareItems applicationActivities:nil];
+	//if iPhone
+	if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+		[root_controller presentViewController:avc animated:YES completion:nil];
+	}
+	//if iPad
+	else {
+	// Change Rect to position Popover
+		UIPopoverController *popup = [[UIPopoverController alloc] initWithContentViewController:avc];
+		[popup presentPopoverFromRect:CGRectMake(root_controller.view.frame.size.width/2, root_controller.view.frame.size.height/4, 0, 0)inView:root_controller.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+	}
+}
+
+void InAppStore::share_image(const String &path, const String &title, const String &subject, const String &text) {
+	AppDelegate *app_delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+	ViewController *root_controller = (ViewController *)[app_delegate viewController];
+	
+	NSString * message = [NSString stringWithCString:text.utf8().get_data() encoding:NSUTF8StringEncoding];
+	NSString * imagePath = [NSString stringWithCString:path.utf8().get_data() encoding:NSUTF8StringEncoding];
+
+	UIImage *image = [UIImage imageWithContentsOfFile:imagePath];
+
+	NSArray * shareItems = @[message, image];
+
+	UIActivityViewController * avc = [[UIActivityViewController alloc] initWithActivityItems:shareItems applicationActivities:nil];
+	 //if iPhone
+	if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+		[root_controller presentViewController:avc animated:YES completion:nil];
+	}
+	//if iPad
+	else {
+		// Change Rect to position Popover
+		UIPopoverController *popup = [[UIPopoverController alloc] initWithContentViewController:avc];
+		[popup presentPopoverFromRect:CGRectMake(root_controller.view.frame.size.width/2, root_controller.view.frame.size.height/4, 0, 0)inView:root_controller.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+	}
+}
+
