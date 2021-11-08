15976d26b22a1f546f8acd844e944905a44583b2
Pitanov V.V <dtho47@yandex.ru>
2021-11-08 22:20:53 +1000

    bash

--- scripts/release_xcframework.sh
+++ scripts/release_xcframework.sh
@@ -10,18 +10,19 @@ for lib in $GODOT_PLUGINS; do
     echo " - GENERATE XCFRAMEWORKS ${lib} ${1}"
     ./scripts/generate_xcframework.sh $lib release $1
     ./scripts/generate_xcframework.sh $lib release_debug $1
-    mv -f ./bin/${lib}.release_debug.xcframework ./bin/${lib}.debug.xcframework
+    mv -uf ./bin/${lib}.release_debug.xcframework ./bin/${lib}.debug.xcframework
 done
 
+
+echo "3) MOVE FILES"
 # Move to release folder
 rm -rf ./bin/release
 mkdir -p ./bin/release
 
-echo "3) MOVE FILES"
 # Move Plugin
 for lib in $GODOT_PLUGINS; do
     echo " - MOVE ${lib}"
     mkdir -p ./bin/release/${lib}
-    mv -f ./bin/${lib}.{release,debug}.xcframework ./bin/release/${lib}
-    cp -f ./plugins/${lib}/${lib}.gdip ./bin/release/${lib}
+    mv -uf ./bin/${lib}.{release,debug}.xcframework ./bin/release/${lib}
+    cp -uf ./plugins/${lib}/${lib}.gdip ./bin/release/${lib}
 done
