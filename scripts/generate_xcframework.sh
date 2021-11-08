0503a5fa6859f1988516a6c5b0cbc2092859ae47
Pitanov V.V <dtho47@yandex.ru>
2021-11-08 21:41:54 +1000

    bash

--- scripts/generate_xcframework.sh
+++ scripts/generate_xcframework.sh
@@ -3,6 +3,8 @@ set -e
 
 # Compile static libraries
 
+echo "RUN SCONS ${1}"
+
 # ARM64 Device
 scons target=$2 arch=arm64 plugin=$1 version=$3
 # ARM7 Device
@@ -14,9 +16,13 @@ scons target=$2 arch=arm64 simulator=yes plugin=$1 version=$3
 
 # Creating a fat libraries for device and simulator
 # lib<plugin>.<arch>-<simulator|iphone>.<release|debug|release_debug>.a
+
+echo "LIPO ${1}"
+
 lipo -create "./bin/lib$1.x86_64-simulator.$2.a" "./bin/lib$1.arm64-simulator.$2.a" -output "./bin/$1-simulator.$2.a"
 lipo -create "./bin/lib$1.armv7-iphone.$2.a" "./bin/lib$1.arm64-iphone.$2.a" -output "./bin/$1-device.$2.a"
 
+echo "CREATE XCFRAMEWORK ${1}"
 # Creating a xcframework 
 xcodebuild -create-xcframework \
     -library "./bin/$1-device.$2.a" \
